28d128b0fa5c84645190fc6ba38e72298645e611
landa44 <pablo44landa@gmail.com>
2022-12-08 17:09:30 -0500

    documented code

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -3,6 +3,13 @@ package tech.strategio;
 import java.util.Scanner;
 
 public class LoanCalculator {
+    /**
+     * Calculate the remaining amount of money of a loan
+     * after n months paying 10% each month.
+     * @param amount Amount of the loan
+     * @param months number of months paying
+     * @return Remaining amount after paid months
+     */
     static int getRemainingAmount(int amount, int months){
         for(int i = 0; i < months; i++){
             amount -= amount/10;
@@ -12,10 +19,10 @@ public class LoanCalculator {
 
     /**
      * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * Calculate the remaining amount of money of a loan
+     * after 3 months paying 10% each month.
+     * @param amount Amount of the loan
+     * @return Remaining amount in 3 months
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Rewrite this method
